ed7f59fe325082f4dc7640cbe28ebb623a86ea43
EllenWiderstrand <ellenwi@student.chalmers.se>
2021-10-20 12:00:31 +0200

    add content to the design process

--- src/Process.js
+++ src/Process.js
@@ -5,66 +5,115 @@ import {
     VerticalTimeline,
     VerticalTimelineElement,
   } from "react-vertical-timeline-component"
+import research from './images/socialmedia.png';
+import wireframe1 from './images/wireframe1.png';
+import storyboard from './images/storyboard.png';
+import colourScheme from './images/colourScheme.png';
+import bloglovin from './images/bloglovin.png';
+import wireframe2 from './images/wireframe2.png';
+import wireframe3 from './images/wireframe3.png';
+import wireflow from './images/wireflow.png';
+import responsive from './images/responsive.png';
+import eval1 from './images/eval1.png';
+import eval2 from './images/eval2.png';
 
   let timelineElements = [
     {
       id: 1,
-      title: "Frontend Developer",
-      location: "Dragontail, Ascana",
-      description:
-        "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
-      buttonText: "View Frontend Projects",
-      icon: "1",
+      title: "Research",
+      description: [
+        "6 semi-structured interviews",
+        "Research and benchmarking of competitors and social media"
+      ],
+      images: [
+        {
+          image: research,
+          id: 1,
+        },
+        {
+          image: bloglovin,
+          id: 2,
+        },
+      ],
     },
     {
       id: 2,
-      title: "Backend Developer",
-      location: "Skystead, Craonia",
-      description:
-        "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
-      buttonText: "View Backend Projects",
-      icon: "2",
+      title: "Ideation",
+      description: [
+        "3 directions (2 webites, 1 app)",
+        "Moodboards",
+        "Wireframes",
+        "Journey map",
+        "Colour schemes",
+      ],
+      images: [
+        {
+          image: wireframe1,
+          id: 1,
+        },
+        {
+          image: wireframe2,
+          id: 2,
+        },
+        {
+          image: wireframe3,
+          id: 3,
+        },
+      ]
     },
     {
       id: 3,
-      title: "Quality Assurance Engineer",
-      location: "South Warren, Geshington",
-      description:
-        "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
-      buttonText: "Company Website",
-      icon: "work",
+      title: "Defining",
+      description: [
+        "3 directions → 2 directions",
+        "Focus on website because it is more accessible (can be viewed on both computers and smartphones)",
+        "Wireframes",
+        "Wireflow",
+        "Responsive design",
+        "Storyboard",
+      ],
+      images: [
+        {
+          image: storyboard,
+          id: 1,
+        },
+        {
+          image: wireflow,
+          id: 2,
+        },
+        {
+          image: responsive,
+          id: 3,
+        },
+      ]
     },
     {
       id: 4,
-      title: "Oak Ridge College",
-      location: "South Warren, Geshington",
-      description:
-        "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      buttonText: "Course Certificate",
-      icon: "school",
-    },
-    {
-      id: 5,
-      title: "Hawking College",
-      location: "Skystead, Craonia",
-      description:
-        "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      buttonText: "College Projects",
-      icon: "school",
-    },
-    {
-      id: 6,
-      title: "Marble Hills Grammar School",
-      location: "Dragontail, Ascana",
-      description:
-        "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      icon: "school",
+      title: "Evaluation",
+      description: [
+        "2 interviews with wireframes",
+        "Questionnaire for colour scheme",
+      ],
+      images: [
+        {
+          image: colourScheme,
+          id: 1,
+        },
+        {
+          image: eval1,
+          id: 2,
+        },
+        {
+          image: eval2,
+          id: 2,
+        },
+      ]
     },
   ]
 
 function Process() {
   return (
-    <div>
+    <div className="process">
       <VerticalTimeline>
         {timelineElements.map(element => {
           return (
@@ -76,10 +125,18 @@ function Process() {
               <h3 className="vertical-timeline-element-title">
                 {element.title}
               </h3>
-              <h5 className="vertical-timeline-element-subtitle">
-                {element.location}
-              </h5>
-              <p id="description">{element.description}</p>
+              <ul>
+                {element.description.map(item => {
+                  return (
+                    <li key={item}>{item}</li>
+                  )
+                })}
+              </ul>
+              {element.images.map(item => {
+                  return (
+                    <img alt={element.title} className="processImg" key={item.id} src={item.image} />
+                  )
+                })}
             </VerticalTimelineElement>
           )
         })}
