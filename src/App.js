1753172591f2ca10957ec19ebd028bd26c3a107d
Kajsa <bjarang@student.chalmers.se>
2021-10-20 11:48:53 +0200

    add the design as an png

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './KeepUp.png';
+import design from './theDesign.png';
 import Process from './Process';
 
 function App() {
@@ -29,8 +30,7 @@ function App() {
           constantly reminds users to check different platforms [3].  This has resulted in many people being addicted and KeepUp aims to keep social media fun and stop 
           it from being an addiction.</p>
           
-        <h2 className="heading">The Design</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img alt="Logo for KeepUp" className="design" src={design} />
         <h2 className="heading">Design Process</h2>
         <Process/>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
