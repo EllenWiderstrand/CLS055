4d257c347d5134708c68a848746d389c24fad5de
EllenWiderstrand <ellenwi@student.chalmers.se>
2021-10-21 15:17:23 +0200

    replace the image for the design and move out its heading

--- src/Design.js
+++ src/Design.js
@@ -4,7 +4,13 @@ import design from './images/theDesign.png';
 function Design() {
   return (
     <div className="wrapper">
-        <img alt="The design" className="design" src={design} />
+      <h2 className="heading">The design</h2>
+      <p>
+        This section presents screenshots of the final design and explanations of functionalities and details. 
+        Arrows and numbers are added to get an understanding of the users flow. Additionally, a storyboard of 
+        the usage are added at the end of this section.
+      </p>
+      <img alt="The design" className="design" src={design} />
     </div>
   );
 }
