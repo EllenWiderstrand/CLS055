c27cd41952b063d16e821158642c1fd59ddfbe78
Viktoria Enderstein <vikend@student.chalmser.se>
2021-10-21 15:31:33 +0200

    added resources

--- src/Resources.js
+++ src/Resources.js
@@ -5,6 +5,14 @@ function Resources() {
     <div className="wrapper">
       <h2 className="heading">Resources</h2>
       {/* Put content here */}
+      <p>  
+      [1]  ​​TechLead, “Apps are dead... what's the next big thing?”, <i>YouTube</i>, May. 18, 2020. [Video]. Available:  <a href="url">https://www.youtube.com/watch?v=wOb0ThditOo&t=2s</a> </p>
+      <p>  
+      [2] Buildfire, “Mobile App Download Statistics & Usage Statistics (2021)”, 2021. [Online]. Available: <a href="url"> https://buildfire.com/app-statistics/ </a>  </p>
+      <p>  
+      [3] Feeldoppel, “Notifications Are Stressful - How To Beat Phone Stress”, Nov, 5, 2019. [Online] Available: <a href="url">https://feeldoppel.co.uk/blogs/news/notifications-are-stressful-how-to-beat-phone-stress   </a>     </p>
+      <p>  
+      [4] G, Collins. “Why the infinite scroll is so addictive”,<i> UX Collective</i>. Dec. 10, 2020. [Online]. Available:  <a href="url"> https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5  </a>      </p>
     </div>
   );
 }
