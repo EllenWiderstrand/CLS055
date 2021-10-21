d08ce7aa3f8835b6eace8218763a2f0b6509723c
Kajsa <bjarang@student.chalmers.se>
2021-10-21 15:32:07 +0200

    updated the design

--- src/Design.js
+++ src/Design.js
@@ -4,6 +4,10 @@ import design from './images/theDesign.png';
 function Design() {
   return (
     <div className="wrapper">
+      <h2 className="heading">The Design</h2>
+        <p>This section presents screenshots of the final design and explanations of functionalities and details. 
+          Arrows and numbers are added to get an understanding of the users flow. 
+          Additionally, a storyboard of the usage are added at the end of this section.</p>
         <img alt="The design" className="design" src={design} />
     </div>
   );
