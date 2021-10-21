f96e4eef326c3e288325c22c36ef2371a41b0ca2
EllenWiderstrand <ellenwi@student.chalmers.se>
2021-10-21 21:50:09 +0200

    update content for result

--- src/Result.js
+++ src/Result.js
@@ -1,55 +1,85 @@
 import './App.css';
 import application from './images/applications.png';
 import settings from './images/settings.png';
-import feedStart from './images/feedstart.png';
-import feedEnd from './images/feedEnd.png';
-import feedOld from './images/feedOld.png';
+import likes from './images/likes.png';
+import updates from './images/Updates.png';
+import morePosts from './images/MorePosts.png';
+import colour1 from './images/Colour1.png';
+import finalColour from './images/finalColour.png';
 
 function Result () {
     return(
         <div className="wrapper"> 
             <h2 className="heading">Result</h2>
             <p>
-                KeepUps original purpose is to take control and collect all social media content into one platform and thereby reduce stress and addiction to social media. 
-                The interview of peoples use of social media  resulted in all the interviewees practically using the exact same applications on a daily basis, see figure 4.1.  
-                These statistics correlate quite well with the Statistics stated in “Mobile App Download Statistics and Usage Statistics“ [2]. KeepUp therefore gives the user 
-                the possibility to decide what media to include which make it as suitable as possible for all the users. Hopefully KeepUp then achieves the main core of the purpose 
-                of letting the user take control and gather all social media.  
+                KeepUp's main purpose is to let users take control over their social media usage and collect all content in one platform and, 
+                thereby, reduce stress and addiction to social media. The interviews that were held regarding peoples use of social media, 
+                showed that all interviewees use the same applications on a daily basis. Figure 4.1 shows which social media they use. 
+                These statistics correlate quite well with the Statistics stated in “Mobile App Download Statistics and Usage Statistics“ [2]. 
+                KeepUp, therefore, gives the user the possibility to decide which social media to include which makes it tailored for the users. 
+                Thereby, KeepUp hopefully achieves the main core of the purpose by letting the user take control and gather all social media 
+                on one website.
             </p>
 
             <img alt="Logos for different social media." className="icons" src={application} />
             <p className="figureText">Figure 4.1: Social media applications used by the interviewees.</p>
 
             <p>
-                The purpose of KeepUp is also to try to reduce stress and addiction to social media. KeepUp includes features such as per default the setting is that the user 
-                will not get any notification when anything new has been added or uploaded. KeepUp lets the user actively decide whether or not he or she wants to get notifications
-                instead of forcing the user to actively remove the notifications. With this feature the goal is to reduce some of the stress created by social media, see figure 4.2
+                The purpose of KeepUp is also to reduce stress and addiction to social media. The settings have, therefore, been configured 
+                in a way to cause the user as little stress as possible. One such setting is notifications, where the default setting is to 
+                have them turned off. By forcing the user to actively turn them on if they want notifications, they are less likely to have 
+                them on. Another common feature on social media is to show the number of likes a post gets. In KeepUp, the default settings 
+                for likes is to not show the number. The user can still see that people have liked or reacted to the post but can not see 
+                how many likes/reactions the post has got. Both of these settings can be seen in Figure 4.2 and a post where the number of 
+                likes is hidden can be seen in Figure 4.3.
             </p>
       
-            <img alt="Settings in KeepUp." className="settingsImg" src={settings} />
-            <p className="figureText">Figure 4.2: Settings for an acount in KeepUp.</p>
+            <div className="twoCols">
+                <div className="alignCenter">
+                    <img alt="Settings in KeepUp." className="settingsImg" src={settings} />
+                    <p className="figureText">Figure 4.2: Settings for an account in KeepUp.</p>
+                </div>
+                <div className="alignCenter">
+                    <img alt="A post in KeepUp's feed." className="wrapper" src={likes} />
+                    <p className="figureText">Figure 4.3: A post that does not show the number of likes.</p>
+                </div>
+            </div>
 
             <p>
-                Social media and the addictive features of likes have been removed with KeepUp. Again per default the user only sees that people like different posts but could never 
-                see by how many. Similarly this feature could be changed but then the user has to actively go in and add the feature instead of remove it, see figure 4.3. The feed of 
-                KeepUp also differs from common feeds on the social media market. It is common to include an infinite scrolling possibility but with KeepUp the user is clearly 
-                informed of what is new or not, see figure 4.3 [4]. When the user has scroll through all the new posts the feed ends and the user actively has to click to show all 
-                the old posts in the feed, see figure 4.5. Keep Up therefore gives the user immediately information about how many posts exactly the user has missed but also gives 
-                the user closure with a clear and distinct end of the feed.
+                Another feature where KeepUp differs from common feeds on the social media market is that the feed does not include 
+                infinite scrolling since it can be very addictive [4]. When a user opens KeepUp, it is clearly stated how many new 
+                updates there are, as shown in Figure 4.4. When the user has scrolled through all new posts, the feed ends and the 
+                user is faced with a button that has to be actively clicked on to show previously viewed posts. The button can be 
+                seen in Figure 4.5. With KeepUp, it is, therefore, less likely that the user keeps scrolling past the new posts as 
+                it gives the user closure with a clear and distinct end of the feed.
             </p>
-          
-            <img alt="Application statistics1" className="statistics1" src={feedStart} />
-            <img alt="Application statistics1" className="statistics1" src={feedEnd} />
-            <img alt="Application statistics1" className="statistics1" src={feedOld} />
+
+            <img alt="The number of new updates in the feed" className="computer" src={updates} />
+            <p className="figureText">Figure 4.4: The number of new updates in the feed.</p>
+
+            <img alt="Button for showing more posts" className="computer" src={morePosts} />
+            <p className="figureText">Figure 4.5: The end of the feed for new posts with containing a button to show more posts.</p>
 
             <p>
-                During the ideation phase we started exploring different colour schemes. Every palette had one core aspect in mind; to be calming. One colour palette was towards a blue 
-                scale, see figure 4.6, and the two other towards a purple/brown/beige scheme, see figure 4.7 and 4.8. Since two of the color palettes were quite similar we made a mix of 
-                the two purple/brown/beige palette. With the new two  color schemes we asked several people what they felt about them. This resulted in people liking that both schemes 
-                focused on natural colours which was appreciated since that radiated calmness. Due to the earthy colors in the purple/brown/beige scheme and some more people who preferred 
-                it we continued with it. In this phase the color scheme didn’t have so much contrast the scheme was updated a little with help of the website called webaim to check how 
-                good or bad the contrast was and based on that updated. The final colour scheme was highly appreciated by one person and he/she said it gave a calming and relaxing feeling. 
+                Colours can create different feelings and it was, therefore, important to find colours that did not feel stressful, 
+                but rather gave a calming impression for the users. The final colour scheme used for KeepUp is a result of three different 
+                palettes, as seen in Figure 4.6, that was scaled down to two which finally resulted in one after feedback had been 
+                received. One person said that the chosen colours gave a calming and relaxing feeling and they were appreciated by 
+                everyone who gave feedback. The chosen palette was slightly altered to create a higher contrast between the colours and 
+                the result is seen in Figure 4.7. It is important that KeepUp is as accessible as possible so that as many people as 
+                possible can use it and one way of making it more accessible was to use high contrast between text and background.  
             </p>
+
+            <div className="twoCols">
+                <div className="alignCenter">
+                    <img alt="First colour schemes" className="settingsImg" src={colour1} />
+                    <p className="figureText">Figure 4.6: The first three colour schemes.</p>
+                </div>
+                <div className="alignCenter">
+                    <img alt="Final colour scheme" className="settingsImg" src={finalColour} />
+                    <p className="figureText">Figure 4.7: The final colour scheme.</p>
+                </div>
+            </div>
         </div>
     )
 }
