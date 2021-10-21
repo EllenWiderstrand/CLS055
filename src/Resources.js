88faa994d33516b986baba99f2f7ece2911e2729
EllenWiderstrand <ellenwi@student.chalmers.se>
2021-10-21 21:58:15 +0200

    fix href for resources

--- src/Resources.js
+++ src/Resources.js
@@ -6,13 +6,13 @@ function Resources() {
       <h2 className="heading">Resources</h2>
       {/* Put content here */}
       <p>  
-      [1]  ​​TechLead, “Apps are dead... what's the next big thing?”, <i>YouTube</i>, May. 18, 2020. [Video]. Available:  <a href="url">https://www.youtube.com/watch?v=wOb0ThditOo&t=2s</a> </p>
+      [1]  ​​TechLead, “Apps are dead... what's the next big thing?”, <i>YouTube</i>, May. 18, 2020. [Video]. Available:  <a href="https://www.youtube.com/watch?v=wOb0ThditOo&t=2s" target="blank">https://www.youtube.com/watch?v=wOb0ThditOo&t=2s </a></p>
       <p>  
-      [2] Buildfire, “Mobile App Download Statistics & Usage Statistics (2021)”, 2021. [Online]. Available: <a href="url"> https://buildfire.com/app-statistics/ </a>  </p>
+      [2] Buildfire, “Mobile App Download Statistics & Usage Statistics (2021)”, 2021. [Online]. Available: <a href="https://buildfire.com/app-statistics/" target="blank"> https://buildfire.com/app-statistics/</a></p>
       <p>  
-      [3] Feeldoppel, “Notifications Are Stressful - How To Beat Phone Stress”, Nov, 5, 2019. [Online] Available: <a href="url">https://feeldoppel.co.uk/blogs/news/notifications-are-stressful-how-to-beat-phone-stress   </a>     </p>
+      [3] Feeldoppel, “Notifications Are Stressful - How To Beat Phone Stress”, Nov, 5, 2019. [Online] Available: <a href="https://feeldoppel.co.uk/blogs/news/notifications-are-stressful-how-to-beat-phone-stress" target="blank">https://feeldoppel.co.uk/blogs/news/notifications-are-stressful-how-to-beat-phone-stress </a></p>
       <p>  
-      [4] G, Collins. “Why the infinite scroll is so addictive”,<i> UX Collective</i>. Dec. 10, 2020. [Online]. Available:  <a href="url"> https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5  </a>      </p>
+      [4] G, Collins. “Why the infinite scroll is so addictive”,<i> UX Collective</i>. Dec. 10, 2020. [Online]. Available: <a href="https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5" target="blank"> https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5 </a></p>
     </div>
   );
 }
