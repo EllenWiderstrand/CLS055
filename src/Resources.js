bbc247d6d4d12cb2bc8420ec6d07177889b6bbaf
EllenWiderstrand <ellenwi@student.chalmers.se>
2021-10-22 09:38:21 +0200

    add resources for result

--- src/Resources.js
+++ src/Resources.js
@@ -12,12 +12,14 @@ function Resources() {
       <p>  
       [3] Feeldoppel, “Notifications Are Stressful - How To Beat Phone Stress”, Nov, 5, 2019. [Online] Available: <a href="https://feeldoppel.co.uk/blogs/news/notifications-are-stressful-how-to-beat-phone-stress" target="blank">https://feeldoppel.co.uk/blogs/news/notifications-are-stressful-how-to-beat-phone-stress </a></p>
       <p>  
-      [4] G, Collins. “Why the infinite scroll is so addictive”,<i> UX Collective</i>. Dec. 10, 2020. [Online]. Available: <a href="https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5" target="blank"> https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5 </a></p>
+      [4] S.Knispel, “Getting fewer ‘likes’ on social media can make teens anxious and depressed” <i>University of Rochester</i>, September. 2020. Accessed on: Oct. 22. 2021. [Online]. Available: <a href="https://www.rochester.edu/newscenter/getting-fewer-likes-on-social-media-can-make-teens-anxious-and-depressed-453482/" target="blank"> https://www.rochester.edu/newscenter/getting-fewer-likes-on-social-media-can-make-teens-anxious-and-depressed-453482/ </a></p>
+      <p>  
+      [5] G, Collins. “Why the infinite scroll is so addictive”,<i> UX Collective</i>. Dec. 10, 2020. [Online]. Available: <a href="https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5" target="blank"> https://uxdesign.cc/why-the-infinite-scroll-is-so-addictive-9928367019c5 </a></p>
       <p>
-      [5] K. Cherry, <i>Can Color Affect Your Mood and Behavior?</i>, Verywell Mind, May. 2020. Accessed on: Oct. 21. 2021. [Online]. Available: <a href="https://www.verywellmind.com/color-psychology-2795824" target="blank"> https://www.verywellmind.com/color-psychology-2795824 </a> 
+      [6] K. Cherry, <i>Can Color Affect Your Mood and Behavior?</i>, Verywell Mind, May. 2020. Accessed on: Oct. 21. 2021. [Online]. Available: <a href="https://www.verywellmind.com/color-psychology-2795824" target="blank"> https://www.verywellmind.com/color-psychology-2795824 </a> 
       </p>
       <p>
-      [6] Flux, “Accessible Web Design: What Is It & How To Do It,” <i>YouTube</i>, Nov. 4, 2019. [Video]. Available: <a href="https://www.youtube.com/watch?v=-ao_Kc_8rpE" target="blank"> https://www.youtube.com/watch?v=-ao_Kc_8rpE </a> 
+      [7] Flux, “Accessible Web Design: What Is It & How To Do It,” <i>YouTube</i>, Nov. 4, 2019. [Video]. Available: <a href="https://www.youtube.com/watch?v=-ao_Kc_8rpE" target="blank"> https://www.youtube.com/watch?v=-ao_Kc_8rpE </a> 
       </p>
     </div>
   );
